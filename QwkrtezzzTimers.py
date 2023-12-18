fb93ca3060c202e7d3f4df840101dd5a101bb220
Nikita Akumov <50026919+nubovik01@users.noreply.github.com>
2023-12-19 01:10:11 +0300

    Update QwkrtezzzTimers.py

--- QwkrtezzzTimers.py
+++ QwkrtezzzTimers.py
@@ -1,1 +1,38 @@
-t
+#                  _         _                   
+#    __ ___      _| | ___ __| |_ ___ ____________
+#   / _` \ \ /\ / / |/ / '__| __/ _ \_  /_  /_  /
+#  | (_| |\ V  V /|   <| |  | ||  __// / / / / / 
+#   \__, | \_/\_/ |_|\_\_|   \__\___/___/___/___|
+#      |_|                                       
+#                 © Copyright 2023
+#              https://t.me/qwkrtezzz
+#                   
+# 🔒         Licensed under the GNU AGPLv3
+# 🌐   https://www.gnu.org/licenses/agpl-3.0.html
+#
+# meta developer: @nbvkxd
+
+from .. import loader, utils
+from datetime import datetime
+
+def getTime(month, day):
+  now = datetime.now()
+  date = datetime(now.year, month, day)
+
+  if now.month > month or (now.month == month and now.day > day):
+    date = datetime(now.year+1, month, day)
+    timeToIt = abs(date-now)
+
+  return timeToIt
+
+class QwkrtezzzTimers(loader.Module):
+  '''Таймеры до начала определённых событий'''
+
+  strings = {'name': 'QwkrtezzzTimers'}
+
+  @loader.command()
+  async def summertime(self, message):
+    """узнать сколько времени осталось до лета"""
+    timeToSummer = getTime(6, 1)
+
+    await utils.answer(message, (f'До лета осталось {timeToSummer.days} дней, {timeToSummer.seconds // 3600} часов, {timeToSummer.seconds // 60 % 60} минут, {timeToSummer.seconds % 60} секунд.\n'))
