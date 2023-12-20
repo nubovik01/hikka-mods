2d767bf23822301a050ef53ac8fea971849f7eb7
Nikita Akumov <50026919+nubovik01@users.noreply.github.com>
2023-12-20 23:50:04 +0300

    some changes and updates.

--- QwkrtezzzTimers.py
+++ QwkrtezzzTimers.py
@@ -9,7 +9,7 @@
 #                   
 # 🔒         Licensed under the GNU AGPLv3
 # 🌐   https://www.gnu.org/licenses/agpl-3.0.html
-#
+
 # meta developer: @nbvkxd
 
 from .. import loader, utils
@@ -21,10 +21,11 @@ def getTime(month, day):
 
   if now.month > month or (now.month == month and now.day > day):
     date = datetime(now.year+1, month, day)
-  
-  timeToIt = abs(date-now)
 
-  return timeToIt
+  return abs(date-now)
+
+def getTimezone():
+  return datetime.now().astimezone().tzinfo
 
 @loader.tds
 class QwkrtezzzTimers(loader.Module):
@@ -32,22 +33,24 @@ class QwkrtezzzTimers(loader.Module):
 
   strings_ru = {
     "name": "QwkrtezzzTimers",
-    "timeUntilSpring": "До весны осталось {} дней, {} часов, {} минут, {} секунд",
-    "timeUntilSummer": "До лета осталось {} дней, {} часов, {} минут, {} секунд",
-    "timeUntilAutumn": "До осени осталось {} дней, {} часов, {} минут, {} секунд",
-    "timeUntilWinter": "До зимы осталось {} дней, {} часов, {} минут, {} секунд",
-    "timeUntilQwzBirthday": "До дня рождения разработчика модуля QwkrtezzzTimers осталось {} дней, {} часов, {} минут, {} секунд 🎁 🎉",
-    "timeUntilNewYear": "До Нового Года осталось {} дней, {} часов, {} минут, {} секунд"
+    "timeUntilEvent": "До {} осталось {}d, {}h, {}m, {}s ({}) {}",
+    "spring": "весны 🏖",
+    "summer": "лета ☀️",
+    "autumn": "осени 🍁",
+    "winter": "зимы ❄️ ☃️",
+    "newYear": "нового года 🎄",
+    "qwzBirthday": "дня рождения разработчика модуля QwkrtezzzTimers 🎉 🎁"
   }
 
   strings_en = {
     "name": "QwkrtezzzTimers",
-    "timeUntilSpring": "{} days, {} hours, {} minutes, {} seconds left until spring",
-    "timeUntilSummer": "{} days, {} hours, {} minutes, {} seconds left until summer",
-    "timeUntilAutumn": "{} days, {} hours, {} minutes, {} seconds left until autumn",
-    "timeUntilWinter": "{} days, {} hours, {} minutes, {} seconds left until winter",
-    "timeUntilQwzBirthday": "There are {} days, {} hours, {} minutes, {} seconds left until the birthday of the QwkrtezzzTimers module developer",
-    "timeUntilNewYear": "{} days, {} hours, {} minutes, {} seconds left until New Year",
+    "timeUntilEvent": "Until {} left {}d, {}h, {}m, {}s ({}) {}",
+    "spring": "spring 🏖",
+    "summer": "summer ☀️",
+    "autumn": "autumn 🍁",
+    "winter": "winter ❄️ ☃️",
+    "newYear": "New Year 🎄",
+    "qwzBirthday": "the birthday of the QwkrtezzzTimers module developer 🎉 🎁",
     "_cmd_doc_springtime": "Find out how much time is left until spring",
     "_cmd_doc_summertime": "Find out how much time is left until summer",
     "_cmd_doc_autumntime": "Find out how much time is left until autumn",
@@ -61,34 +64,82 @@ class QwkrtezzzTimers(loader.Module):
     """Узнать сколько времени осталось до весны"""
     timeToSpring = getTime(3, 1)
 
-    await utils.answer(message, self.strings("timeUntilSpring").format(timeToSpring.days, timeToSpring.seconds // 3600, timeToSpring.seconds // 60 % 60, timeToSpring.seconds % 60))
+    await utils.answer(message, self.strings("timeUntilEvent").format(
+      self.strings("spring"),
+      timeToSpring.days,
+      timeToSpring.seconds // 3600,
+      timeToSpring.seconds // 60 % 60, 
+      timeToSpring.seconds % 60,
+      getTimezone(),
+      utils.ascii_face()
+    ))
 
   async def summertimecmd(self, message):
     """Узнать сколько времени осталось до лета"""
     timeToSummer = getTime(6, 1)
 
-    await utils.answer(message, self.strings("timeUntilSummer").format(timeToSummer.days, timeToSummer.seconds // 3600, timeToSummer.seconds // 60 % 60, timeToSummer.seconds % 60))
+    await utils.answer(message, self.strings("timeUntilEvent").format(
+      self.strings("summer"),
+      timeToSummer.days, 
+      timeToSummer.seconds // 3600, 
+      timeToSummer.seconds // 60 % 60, 
+      timeToSummer.seconds % 60,
+      getTimezone(),
+      utils.ascii_face()
+    ))
 
   async def autumntimecmd(self, message):
     """Узнать сколько времени осталось до осени"""
     timeToAutumn = getTime(9, 1)
 
-    await utils.answer(message, self.strings("timeUntilAutumn").format(timeToAutumn.days, timeToAutumn.seconds // 3600, timeToAutumn.seconds // 60 % 60, timeToAutumn.seconds % 60))
+    await utils.answer(message, self.strings("timeUntilEvent").format(
+      self.strings("autumn"),
+      timeToAutumn.days,
+      timeToAutumn.seconds // 3600,
+      timeToAutumn.seconds // 60 % 60,
+      timeToAutumn.seconds % 60,
+      getTimezone(),
+      utils.ascii_face()
+    ))
 
   async def wintertimecmd(self, message):
     """Узнать сколько времени осталось до зимы"""
     timeToWinter = getTime(12, 1)
 
-    await utils.answer(message, self.strings("timeUntilWinter").format(timeToWinter.days, timeToWinter.seconds // 3600, timeToWinter.seconds // 60 % 60, timeToWinter.seconds % 60))
+    await utils.answer(message, self.strings("timeUntilEvent").format(
+      self.strings("winter"),
+      timeToWinter.days,
+      timeToWinter.seconds // 3600,
+      timeToWinter.seconds // 60 % 60,
+      timeToWinter.seconds % 60,
+      getTimezone(),
+      utils.ascii_face()
+    ))
 
   async def newyeartimecmd(self, message):
     """Узнать сколько времени осталось до нового года"""
     timeToNewYear = getTime(12, 31)
 
-    await utils.answer(message, self.strings("timeUntilNewYear").format(timeToNewYear.days, timeToNewYear.seconds // 3600, timeToNewYear.seconds // 60 % 60, timeToNewYear.seconds % 60))
+    await utils.answer(message,self.strings("timeUntilEvent").format(
+      self.strings("newYear"),
+      timeToNewYear.days, 
+      timeToNewYear.seconds // 3600, 
+      timeToNewYear.seconds // 60 % 60,
+      timeToNewYear.seconds % 60,
+      getTimezone(),
+      utils.ascii_face()
+    ))
 
   async def qwzbirthdaycmd(self, message):
     """Узнать сколько времени осталось до дня рождения разработчика этого модуля (Никиты/Qwkrtezzz)"""
     timeToBirthday = getTime(4, 9)
 
-    await utils.answer(message, self.strings("timeUntilQwzBirthday").format(timeToBirthday.days, timeToBirthday.seconds // 3600, timeToBirthday.seconds // 60 % 60, timeToBirthday.seconds % 60))
+    await utils.answer(message, self.strings("timeUntilEvent").format(
+      self.strings("qwzBirthday"),
+      timeToBirthday.days, 
+      timeToBirthday.seconds // 3600, 
+      timeToBirthday.seconds // 60 % 60, 
+      timeToBirthday.seconds % 60,
+      getTimezone(),
+      utils.ascii_face()
+    ))
